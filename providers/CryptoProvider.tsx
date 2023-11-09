37bd023c6f60e744a7b481e78e652ec040d073c8
Miha Žnidar <mayki.znidar@gmail.com>
2023-11-09 21:57:30 +0100

    Added contract functions to crypto provider

--- providers/CryptoProvider.tsx
+++ providers/CryptoProvider.tsx
@@ -1,5 +1,8 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { mumbaiNetwork } from "../constants";
+import { Swap } from "../contracts/types/Swap";
+import { Swap_factory } from "../contracts/Swap_factory";
+import { JsonRpcProvider, ethers } from "ethers";
 
 interface CryptoContextType {
   account: String;
@@ -8,7 +11,16 @@ interface CryptoContextType {
   connect: () => void;
   disconect: () => void;
   currentNetwork: () => Promise<String>,
-  switchNetwork: (chainId: string) => void;
+  switchNetwork: (chainId: string) => void,
+
+  allSupportedTokens: () => Promise<string[]>,
+  allTokens: () => Promise<string[]>,
+  allSupportedBalances: () => Promise<string[]>,
+  tokenIsSupported: (token: string) => Promise<boolean>,
+  tokenIsActive: (token: string) => Promise<boolean>,
+  tokenBalance: (token: string) => Promise<any>,
+  addToken: (token: string) => void,
+  removeToken: (token: string) => void,
 }
 
 const CryptoContext = createContext<CryptoContextType>({
@@ -19,6 +31,15 @@ const CryptoContext = createContext<CryptoContextType>({
   disconect: () => null,
   currentNetwork: () => Promise.resolve(''),
   switchNetwork: (chainId: string) => null,
+
+  allSupportedTokens: () => Promise.resolve([]),
+  allTokens: () => Promise.resolve([]),
+  allSupportedBalances: () => Promise.resolve([]),
+  tokenIsSupported: (token: string) => Promise.resolve(false),
+  tokenIsActive: (token: string) => Promise.resolve(false),
+  tokenBalance: (token: string) => Promise.resolve({}),
+  addToken: (token: string) => null,
+  removeToken: (token: string) => null,
 })
 
 export const CryptoProvider = ({
@@ -28,6 +49,8 @@ export const CryptoProvider = ({
 }) => {
   const [account, setAccount] = useState<String>('')
   const [network, setNetwork] = useState<String>('')
+  const [swapContract, setSwapContract] = useState<Swap>();
+  const [provider, setProvider] = useState<JsonRpcProvider>(new ethers.JsonRpcProvider(process.env.NEXT_PUBLIC_RPC));
 
   const isConnected = () => {
     if (account !== '') return true
@@ -83,9 +106,79 @@ export const CryptoProvider = ({
     });
   }
 
+  
+  const connectToSwapContract = async () => {
+    const signer = await provider.getSigner()
+    const contract: Swap = Swap_factory.connect(process.env.NEXT_PUBLIC_SWAP_CONTRACT!, signer)
+    setSwapContract(contract)
+  }
+
+  const checkContractInit = () => {
+    if (!swapContract) {
+      console.error('Not connected to contract')
+      return []
+    }
+  }
+
+  const allTokens = async (): Promise<string[]> => {
+    checkContractInit()
+    const tokens = await swapContract!.allTokens()
+    console.log(tokens)
+    return tokens
+  }
+  
+  const allSupportedTokens = async (): Promise<string[]> => {
+    checkContractInit()
+    const tokens = await swapContract!.allSupported()
+    console.log(tokens)
+    return tokens
+  }
+
+  const allSupportedBalances = async () => {
+    checkContractInit()
+    const supportedBalances = await swapContract!.allSupportedBalances()
+    console.log(supportedBalances)
+    return []
+  }
+
+  const tokenIsSupported = async (token: string) => {
+    checkContractInit()
+    return swapContract!.isSupported(token)
+  }
+
+  const tokenIsActive = async (token: string) => {
+    checkContractInit()
+    return swapContract!.isActive(token)
+  }
+
+  const tokenBalance = async (token: string) =>{
+    checkContractInit()
+    const balance = await swapContract!.tokenBalance(token)
+    console.log(balance)
+    return balance
+  }
+
+  // ONLY OWNER - PAYABLE
+  const addToken = async (token: string) => {
+    checkContractInit()
+    await swapContract!.addToken(token)
+  }
+
+  // ONLY OWNER - PAYABLE
+  const removeToken = async (token: string) => {
+    checkContractInit()
+    await swapContract!.addToken(token)
+  }
+
+  useEffect(() => {
+    if (isConnected()) {
+      connectToSwapContract()
+    }
+  },[isConnected()])
+
   return (
     <CryptoContext.Provider
-      value={{account, network, isConnected, connect, disconect, currentNetwork, switchNetwork}}
+      value={{account, network, isConnected, connect, disconect, currentNetwork, switchNetwork, allSupportedTokens, allTokens, allSupportedBalances, tokenIsSupported, tokenIsActive, tokenBalance, addToken, removeToken}}
     >
       {children}
     </CryptoContext.Provider>
